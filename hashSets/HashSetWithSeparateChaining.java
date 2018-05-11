50c84cb3285d390afe1478a8cc130f3b3886ee86
amanali1896 <pamanalionline@gmail.com>
2018-05-11 19:20:05 -0400

    Update HashSetWithSeparateChaining.java

--- hashSets/HashSetWithSeparateChaining.java
+++ hashSets/HashSetWithSeparateChaining.java
@@ -2,7 +2,7 @@
 value exists. To solve this issue we will create an array of linkedList. Also known as Ragged Array(A two dimensional array with variable row lengths).
 */
 public class HashSetWithSeparateChaining {
-    
+
     private LinkedList < String > [] buckets; // String Array of linked lists
     public HashSet(int size) { // default constructor
         buckets = new LinkedList[size];
@@ -13,5 +13,15 @@ public class HashSetWithSeparateChaining {
     private int hashCode(String value) { // this method calculates the hashcode. 
         return value.length();
     }
-  
+    public boolean add(String value) {// in this method we add a value to the hashset.
+        if (!contains(value)) { //sets don't allow repetition of values.
+            int index = hashCode(value) % buckets.length; // finding the index of the hashcode. 
+            LinkedList < String > bucket = buckets[index]; // we locate the index in the memory
+            bucket.addFirst(value); // we add the value at the front of that particular linked list
+            return true;
+        }
+        return false; // if the value already exists then we return false indicating that the value was already there in the set.
+    }
+    
+
 }
