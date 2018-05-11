e1742bf6ce5afefcf3777e5c7f67ef306a297cd2
amanali1896 <pamanalionline@gmail.com>
2018-05-10 22:02:08 -0400

    Update Stack.java

--- Stacks using collections api/Stack.java
+++ Stacks using collections api/Stack.java
@@ -1,1 +1,15 @@
-
+import java.util.LinkedList; // we import the linked list class
+public class Stack {
+    protected LinkedList list = new LinkedList();// we create a linked list object
+    public void push(Object value) { // by Push we mean that we have to add the value at the top of the stack. 
+        list.addFirst(value); // we use the inbuilt method to add the value on the top of the stack.
+    }
+    public Object pop() {
+        if (list.isEmpty()) // if the list is empty then return the null object.
+            return null;
+        return list.removeFirst();
+    }
+    public Object peek() { // peek is a function that just looks at the available value on top and returns it value. 
+        return list.getFirst();
+    }
+}
