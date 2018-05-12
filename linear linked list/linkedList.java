87948c778f5fccd68b5b5c2cbf7e34b68ad00aab
Aman Ali <pamanalionline@gmail.com>
2018-05-12 19:43:32 -0400

    Update linkedList.java

--- linear linked list/linkedList.java
+++ linear linked list/linkedList.java
@@ -14,9 +14,12 @@ public class linkedList { // class of the file
     public void addToFront(int value) // here we are creating a new function/ method to
     {
         Node newNode = new Node(value); // create a new node here and add the value
-        newNode.next = head; // next node/ pointer of this new node is pointed towards head pointer which was initially alloted to either null or the the first element
-        head = newNode; // since we are adding the node at the beginning the head pointer is now updated to the address of the new node.
-        if (newNode.next == null) { // this is in a case if there are no elements in the linkedList initially. so this will  allot the tail also to the new node.
+        newNode.next = head; /* next node/ pointer of this new node is pointed towards head pointer which was initially alloted to 
+        either null or the the first element*/
+        head = newNode; /*since we are adding the node at the beginning the head pointer is now 
+        updated to the address of the new node.*/
+        if (newNode.next == null) { /* this is in a case if there are no elements in the linkedList initially. 
+            so this will  allot the tail also to the new node.*/
             tail = newNode;
         }
     }
@@ -47,15 +50,21 @@ public class linkedList { // class of the file
                 tail.next = newNode
                 tail = newNode;
             } else {
-                newNode.next = current.next; // this current node now points at the next node. but now the added value should point at the next node. hence, newNode.next is now equal to the current.next.
+                newNode.next = current.next; /* this current node now points at the next node.
+                but now the added value should point at the next node. hence, newNode.next is now equal to the current.next.*/
+                
                 current.next = newNode; // current.next is now appended to the newly added block
             }
         }
     }
-    public boolean contains(int value) { // this method basically checks if a particular value exists in the linked list or not. Hence the return type is boolean
+    public boolean contains(int value) { /*this method basically checks if a particular value exists in the linked list or not.
+        Hence the return type is boolean*/
+       
         Node current = head; // creating a node current and initialising it to the head pointer/node
         while (current != null) { // traversing the current node til it hits an empty node(null)
-            if (current.value == value) { // if the value of the current node is equal to the value that is asked, then it returns true. which means that the value exists in the list
+            if (current.value == value) { /* if the value of the current node is equal to the value that is asked, 
+                then it returns true. which means that the value exists in the list*/
+                
                 return true;
             }
             current = current.next; // move the current node to the next node
@@ -63,14 +72,19 @@ public class linkedList { // class of the file
         return false; // return false current has traversed and didn't find any value. !!
     }
 
-    public int getByIndex(int index) { // In this method we will try to get the value of the element of a node with the help of its index. We give the input as the index of the node
+    public int getByIndex(int index) { /* In this method we will try to get the value of the element
+    of a node with the help of its index. We give the input as the index of the node*/
+        
         if (index < 0) { // if the input is negative then, it will throw an inbulit exception.
             throw new IndexOutOfBoundsException();
         } else {
             Node current = head; // assign the head node to the current node.
             for (int i = 0; i < index; i++) {
                 if (current == null || current.next == null) {
-                    throw new IndexOutOfBoundsException(); // if the current node traverses til the end and doesn't get the index mentioned. then it will throw an exception. For example, if we search index 7 in a 4 data linked list.
+                    throw new IndexOutOfBoundsException(); /* if the current node traverses til the end and 
+                    doesn't get the index mentioned. then it will throw an exception.
+                    For example, if we search index 7 in a 4 data linked list.*/
+                
                 }
                 current = current.next; // update the node.
             }
@@ -94,7 +108,11 @@ public class linkedList { // class of the file
             head = null;
             tail = null;
         } else {
-            Node current = head; // we create a new node for traversal and we initailise that to head. we will now travel till the next node is tail. we will allocate tail to the previous node and update the next node to null. thus, removing the last element.
+            Node current = head; /* we create a new node for traversal and we initailise that to head.
+            we will now travel till the next node is tail. we will allocate tail to the previous node and 
+            update the next node to null. thus, removing the last element.*/
+           
+            
             while (current.next != tail) {
                 current = current.next;
             }
@@ -106,7 +124,9 @@ public class linkedList { // class of the file
         if (index < 0) { // if the index is negative, then we throw an inbulit exception
             throw new IndexOutOfBoundsException();
         } else if (index == 0) {
-            removeFromFront(); // if the index is zero, which means we have to remove the first element. hence we call the previous function
+            removeFromFront(); /*if the index is zero, which means we have to remove the first element.
+            hence we call the previous function*/
+       
         } else {
             Node current = head; // we create a new node for traversal
             for (int i = 0; i < index - 1; i++) { // we traverse til the respective index. 
@@ -118,7 +138,8 @@ public class linkedList { // class of the file
             if (current.next == null) {
                 throw new IndexOutOfBoundsException();
             }
-            current.next = current.next.next; // we skip the node we need to remove and direct its previous node to the next node available. 
+            current.next = current.next.next; /* we skip the node we need to remove and direct its
+            previous node to the next node available. */
             if (current.next == null) { // if the index node itself is the last node, then we simply assign it to the current node. 
                 tail = current;
             }
