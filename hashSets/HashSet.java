33655ec48fc203973208ad1b1ef4080737fe5ee6
Aman Ali <pamanalionline@gmail.com>
2018-05-12 19:50:39 -0400

    Commenting style changed.

--- hashSets/HashSet.java
+++ hashSets/HashSet.java
@@ -1,16 +1,23 @@
 //Hashsets combines the extensible nature of linkedlist and the searching and information retrival power of an array.
 // we will implement hashsets in this file
-// this is a very naive implementaion. please find another file (HashSetWithSeparateChaining.java) in the same directory for a more sophisticated implementaions.
+/* this is a very naive implementaion. please find another file (HashSetWithSeparateChaining.java) in 
+the same directory for a more sophisticated implementaions.*/
 public class HashSet { // we are creating a classs of hashset.
 
     private String[] values; // here we are declaring a array of strings.
-    private int hashCode(String value) { // this is a method to convert the input data into a hash. This is the hash function. It calculates the hash by calculating the length of the string. for example hash for for aman is "4".
+    private int hashCode(String value) { /* this is a method to convert the input data into a hash. 
+        This is the hash function. It calculates the hash by calculating the length of the string. 
+            for example hash for for aman is "4".*/
         return value.length();
     }
 
     public boolean add(String value) { // this method adds the string into the set
-        int index = hashCode(value) % values.length; // it first generates its hash code. we do modulo operation so that the index is always in the limits and we do not have an out of bounds issue.
-        if (values[index] == null) { // value will be added only if the index is empty. If there is already a value in the index, the it would return false
+        int index = hashCode(value) % values.length; /* it first generates its hash code.
+        we do modulo operation so that the index is always in the limits and we do not have an out of bounds issue.*/
+       
+        
+        if (values[index] == null) { /* value will be added only if the index is empty.
+        If there is already a value in the index, the it would return false*/
             values[index] = value;
             return true;
         }
@@ -19,7 +26,8 @@ public class HashSet { // we are creating a classs of hashset.
     
     public boolean contains(String value) { // this method checks if a value exists in the set.
         int index = hashCode(value) % values.length; // calculates the index of the value
-        return value.equals(values[index]);// checks if the value exists by comparing the parameter passed with the value in the set with the help of .equals() string method.
+        return value.equals(values[index]);/* checks if the value exists by comparing the parameter passed with the 
+        value in the set with the help of .equals() string method.*/
     }
 
 }
