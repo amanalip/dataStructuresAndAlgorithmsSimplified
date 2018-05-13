a2ef8a21549fc0e48285d747b7824806ce900cdd
Aman Ali <pamanalionline@gmail.com>
2018-05-13 00:06:41 -0400

    To be updated with pre and post order traversals.
    Note: This file has to updated with pre order and post order traversals

--- Advanced Data Structures/BinarySearchTree.java
+++ Advanced Data Structures/BinarySearchTree.java
@@ -1,10 +1,11 @@
+/*
+1) Binary search tree is an extension of binary tree data structure.
+2) In a Binary search tree, elements are added in a sorted order. Binary tree intends solve the "preserving the order of data entry"
+problem.
+3) The value of the left child  is always smaller than the parent. The value of the right child is always greater than the parent node.
 
-
-/*Binary tree is a type of tree data structure.In a Binary tree, elements are added in a sorted order. The topmost node is called a root.
-and the other nodes are parent node and child node. The value of the left child  is always smaller than the parent. The value of the
-right child is always greater than the parent node. This file just has the simple implementation of the binary tree. Binary tree intends
-solve the "preserving the order of data entry" problem.*/
-
+Note: This file has to updated with preorder and post order traversals.
+*/
 public class BinarySearchTree {
     class Node {
         int value;
