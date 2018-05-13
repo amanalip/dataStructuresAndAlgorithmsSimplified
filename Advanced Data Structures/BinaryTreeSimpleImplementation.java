cef489b028d8b28094d2da672d2e4fe6446cad17
Aman Ali <pamanalionline@gmail.com>
2018-05-13 00:17:15 -0400

    Update BinaryTreeSimpleImplementation.java

--- Advanced Data Structures/BinaryTreeSimpleImplementation.java
+++ Advanced Data Structures/BinaryTreeSimpleImplementation.java
@@ -8,7 +8,7 @@ public class BinaryTreeSimpleImplementation {
         Node leftChild = null;// initialising the values of left and right child to null.
         Node rightChild = null;
         Node(int value) {
-            this.value = value; 
+            this.value = value; // adding the value to the respective node.
         }
     }
     protected Node root = null; // initialising root to null
