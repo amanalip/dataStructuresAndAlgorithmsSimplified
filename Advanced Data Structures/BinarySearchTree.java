8776cd6c4e0b34dd4ac12428eae6175d1d557ac7
Aman Ali <pamanalionline@gmail.com>
2018-05-12 23:36:22 -0400

    Update BinarySearchTree.java

--- Advanced Data Structures/BinarySearchTree.java
+++ Advanced Data Structures/BinarySearchTree.java
@@ -1,5 +1,9 @@
 
 
+/*Binary tree is a type of tree data structure.In a Binary tree, elements are added in a sorted order. The topmost node is called a root.
+and the other nodes are parent node and child node. The value of the left child  is always smaller than the parent. The value of the
+right child is always greater than the parent node. This file just has the simple implementation of the binary tree. Binary tree intends
+solve the "preserving the order of data entry" problem.*/
 
 public class BinarySearchTree {
     class Node {
