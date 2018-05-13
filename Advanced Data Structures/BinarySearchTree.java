5903313fe9b5bd9de7de50d136a74e5c1e9df8d3
Aman Ali <pamanalionline@gmail.com>
2018-05-12 23:17:55 -0400

    Update BinarySearchTree.java

--- Advanced Data Structures/BinarySearchTree.java
+++ Advanced Data Structures/BinarySearchTree.java
@@ -1,1 +1,23 @@
 
+
+
+public class BinarySearchTree {
+    class Node {
+        int value;
+        Node leftChild = null;
+        Node rightChild = null;
+        Node(int value) {
+            this.value = value;
+        }
+    }
+    protected Node root = null;
+    public void inorderTraversal(Node n) {
+        if (n == null) return;
+        inorderTraversal(n.leftChild);
+        visit(n);
+        inorderTraversal(n.rightChild);
+    }
+    public void visit(Node n) {
+        System.out.println(n.value);
+    }
+}
