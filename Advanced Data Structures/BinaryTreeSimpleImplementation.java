7b4c9f81f367188eb16aebdeb47d593bdd1a52f6
Aman Ali <pamanalionline@gmail.com>
2018-05-12 23:09:19 -0400

    Update BinaryTreeSimpleImplementation.java

--- Advanced Data Structures/BinaryTreeSimpleImplementation.java
+++ Advanced Data Structures/BinaryTreeSimpleImplementation.java
@@ -1,1 +1,17 @@
+/*In a Binary tree, elements are added in a sorted order. The topmost node is called a root. and the other nodes are parent node and
+child node. The value of the left child  is always smaller than the parent. The value of the right child is always greater than the
+parent node. This file just has the simple implementation of the binary tree.*/
 
+
+public class BinaryTree {
+    class Node {
+        int value; //the data that needs to be given.
+        Node leftChild = null;// initialising the values of left and right child to null.
+        Node rightChild = null;
+        Node(int value) {
+            this.value = value; 
+        }
+    }
+    protected Node root = null; // initialising root to null
+  
+}
