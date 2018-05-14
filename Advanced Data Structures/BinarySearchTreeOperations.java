39f713cc133db4faefaf3f2fe374af8de1c32872
Aman Ali <pamanalionline@gmail.com>
2018-05-14 01:45:19 -0400

    Update BinarySearchTreeOperations.java

--- Advanced Data Structures/BinarySearchTreeOperations.java
+++ Advanced Data Structures/BinarySearchTreeOperations.java
@@ -1,1 +1,12 @@
 
+public class BinarySearchTreeOperations {
+    class Node {
+        int value; //the data that needs to be given.
+        Node leftChild = null; // initialising the values of left and right child to null.
+        Node rightChild = null;
+        Node(int value) {
+            this.value = value; // adding the value to the respective node.
+        }
+    }
+    protected Node root = null; // initialising root to null
+}
