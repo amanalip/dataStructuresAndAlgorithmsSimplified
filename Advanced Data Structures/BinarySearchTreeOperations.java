29d7cedb9d35c369f6693bc83b7aa2bbd865ebdc
Aman Ali <pamanalionline@gmail.com>
2018-05-14 21:51:40 -0400

    Update recursion explaination in detail.

--- Advanced Data Structures/BinarySearchTreeOperations.java
+++ Advanced Data Structures/BinarySearchTreeOperations.java
@@ -11,18 +11,42 @@ public class BinarySearchTreeOperations {
     public void visit(Node n) {
         System.out.println(n.value); //This node prints the respective value
     }
-    
+
     public boolean binaryTreeSearch(Node n, int val) { // method is meant for searching the value in a binary search tree.
         if (n == null) return false; // if no such node exists then return false
         if (n.value == val) { // return true if the such a value exists in the tree
             return true;
-        } else if (n.value > val) { /* if the value is lesser than a node, recursively search in the left child.
-        since left children are smaller than the parent. this recursive call continues til the time we either get a value. or 
-        we don't get it.*/
+        } else if (n.value > val) {
+            /* if the value is lesser than a node, recursively search in the left child.
+                   since left children are smaller than the parent. this recursive call continues til the time we either
+                   get a value. or we don't get it.*/
             return binaryTreeSearch(n.leftChild, val);
         } else {
-            return binaryTreeSearch(n.rightChild, val); /* in this condition we will recursively call the function and search in the
-            right child of the tree. We search in the right child because the value to be searched is greater than the required value.*/
+            return binaryTreeSearch(n.rightChild, val);
+            /* in this condition we will recursively call the function and search in the
+                       right child of the tree. We search in the right child because the value to be searched is greater 
+                       than the required value.*/
+        }
+    }
+/* Note: Again the following method has recursion in it. I will explain recursion in detail in another file, and its path would be 
+updated here.*/
+    public boolean add(Node n, int val) { // this method adds the value. 
+        if (n.value == val) { // if value already exists then no duplication allowed.
+            return false;
+        } else if (n.value > val) { //if the value is less than parent then add it to the left child.
+            if (n.leftChild == null) {
+                n.leftChild = new Node(val); // if left child/node doesn't exist create one
+                return true;
+            } else {
+                return add(n.leftChild, val);// otherwise recursively add it to the left subtree
+            }
+        } else { 
+            if (n.rightChild == null) { // if right child is null, create a new value
+                n.rightChild = new Node(val);
+                return true;
+            } else {
+                return add(n.rightChild, val); // recursively add to the new subtree
+            }
         }
     }
 }
