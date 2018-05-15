3827deda827251b0f5124e3884812be1ece13ac0
Aman Ali <pamanalionline@gmail.com>
2018-05-14 20:20:12 -0400

    Search method updated

--- Advanced Data Structures/BinarySearchTreeOperations.java
+++ Advanced Data Structures/BinarySearchTreeOperations.java
@@ -1,4 +1,3 @@
-
 public class BinarySearchTreeOperations {
     class Node {
         int value; //the data that needs to be given.
@@ -9,6 +8,21 @@ public class BinarySearchTreeOperations {
         }
     }
     protected Node root = null; // initialising root to null
+    public void visit(Node n) {
+        System.out.println(n.value); //This node prints the respective value
+    }
+    
+    public boolean binaryTreeSearch(Node n, int val) { // method is meant for searching the value in a binary search tree.
+        if (n == null) return false; // if no such node exists then return false
+        if (n.value == val) { // return true if the such a value exists in the tree
+            return true;
+        } else if (n.value > val) { /* if the value is lesser than a node, recursively search in the left child.
+        since left children are smaller than the parent. this recursive call continues til the time we either get a value. or 
+        we don't get it.*/
+            return binaryTreeSearch(n.leftChild, val);
+        } else {
+            return binaryTreeSearch(n.rightChild, val); /* in this condition we will recursively call the function and search in the
+            right child of the tree. We search in the right child because the value to be searched is greater than the required value.*/
+        }
+    }
 }
-
-
